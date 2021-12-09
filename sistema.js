a78e8ce657035a11bf8befd54d46a812d284270c
fermuniz83 <fercomobone@hotmail.com>
2021-12-09 19:33:43 -0300

    sistemaaluno

---
+++ sistema.js
@@ -0,0 +1,60 @@
+let nomeDeAluno = ["Marcel","Franklin","Rita","Afranio"]
+for (let nome of nomeDeAluno){
+    console.log(`Esta pessoa se chama ${nome}`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let numeroDeAlunos = 10;
+
+// let contador = 0;
+
+// while(contador <= numeroDeAlunos){
+//     if (contador == 0){
+//         console.log("o número atual é zero")
+//     }else if(contador % 2 == 0){
+//         console.log("o número" + contador + "é par")
+//     }else{
+//         console.log(`o número ${contador} é impar`)
+//     }
+//     contador++;
+// }
+    
+        
+     
+    
+
+
+
+
+
+
+
+
+
+
+
+
+// let numeroDeAlunos = 10;
+
+// for (let contador = 0; contador<= numeroDeAlunos; contador++) {
+//   console.log (contador)  
+
+
+//   if(contador== 0){
+//     console.log("o numero atual é zero")
+// } else if(contador % 2 == 0){
+//     console.log("o numero " + contador+" é par")
+// }   else{ console.log (`o número ${contador} é impar`)}
+// }
